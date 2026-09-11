8f5bd29f74b9988cd09f6126ca2423963e29ce74
pstamim <pstamim723@gmail.com>
2026-09-11 21:28:50 +0600

    footer sesh commit

--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -2,37 +2,49 @@
 import Logo from '../assets/logo-text.png'
 const Footer = () => {
     return (
-        
-            <div className='flex justify-between'>
+        <>
+        <div className="divider mt-10 pt-10" />
+            <div className='flex justify-between container mx-auto mt-30'>
+                <div className='flex flex-col space-y-6'>
+                    <img className='h-[40px] w-[160px]' src={Logo} alt="" />
+                    <p className='text-[#64748B]'>Curated tools, technologies, and resources for developers building <br />
+                        modern software.</p>
+                    <ul className='flex gap-4 font-bold mt-10'>
+                        <li><a href="">GitHub</a></li>
+                        <li><a href="">Twitter</a></li>
+                        <li><a href="">LinkedIn</a></li>
+                    </ul>
+                </div>
+                <div className=' flex flex-col gap-2'>
+                    <h3 className=' font-bold '>PRODUCT</h3>
+                    <a className='text-[#64748B]' href="">Home</a>
+                    <a className='text-[#64748B]' href="">Technologies</a>
+                    <a className='text-[#64748B]' href="">Projects</a>
 
-                <img className='h-[40px] w-[160px]' src={Logo} alt="" />
-                <p>Curated tools, technologies, and resources for developers building
-                    modern software.</p>
-                <ul className='flex justify-center'>
-                    <li><a href="">GitHub</a></li>
-                    <li><a href="">Twitter</a></li>
-                    <li><a href="">LinkedIn</a></li>
-                </ul>
-
-                <h3>PRODUCT</h3>
-                <a href="">Home</a>
-                <a href="">Technologies</a>
-                <a href="">Projects</a>
-
-
-                <h3>COMPANY</h3>
-                <a href="">About</a>
-                <a href="">Contact</a>
-                <a href="">Careers</a>
-
-
-                <h3>LEGAL</h3>
-                <a href="">Privacy Policy</a>
-                <a href="">Terms of Service</a>
-
+                </div>
+                <div className='flex flex-col gap-2 '>
+                    <h3 className=' font-bold '>COMPANY</h3>
+                    <a className='text-[#64748B]' href="">About</a>
+                    <a className='text-[#64748B]' href="">Contact</a>
+                    <a className='text-[#64748B]' href="">Careers</a>
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <h3 className=' font-bold '>LEGAL</h3>
+                    <a className='text-[#64748B]' href="">Privacy Policy</a>
+                    <a className='text-[#64748B]' href="">Terms of Service</a>
+                </div>
 
             </div>
-            );
+            <div className="divider container mx-auto mt-10 pt-10" />
+            <div className='flex justify-between container mx-auto mt-20 mb-10'>
+                <p className='text-[#64748B]'>© 2026 Dev Stack. All rights reserved.</p>
+                <div className='flex gap-4 text-[#64748B]'>
+                    <a href="">Privacy</a>
+                    <a href="">Terms</a>
+                </div>
+            </div>
+        </>
+    );
 };
 
-            export default Footer;
+export default Footer;
--- src/Component/Maincart.tsx
+++ src/Component/Maincart.tsx
@@ -1,11 +1,16 @@
 import { FaStar } from "react-icons/fa";
 import type { IExplorCompany } from "./Interface";
+import { useState } from "react";
 
 interface Imaindataprops {
     maindata: IExplorCompany[]
 }
 
 const Maincart = ({ maindata }: Imaindataprops[]) => {
+
+    // const [addStack, setaddStack] = useState(false)
+
+
     return (
         <div>
             <div className="border border-slate-100 rounded-2xl shadow-sm p-6 w-full max-w-sm space-y-3 my-4 items-center">
@@ -14,24 +19,25 @@ const Maincart = ({ maindata }: Imaindataprops[]) => {
                 <div className="flex justify-between ">
 
                     <img className="h-12 w-12" src={maindata.icon} alt="" />
-                    <button className={`${maindata.badgeTextColor} ${maindata.badgeColor} px-5 border border-slate-100 rounded-4xl`}>{maindata.badge}</button>
+                    <button className={`${maindata.badgeTextColor} cursor-pointer ${maindata.badgeColor} px-5 border border-slate-100 rounded-4xl`}>{maindata.badge}</button>
                 </div>
 
                 <h1 className="text-2xl font-bold">{maindata.name}</h1>
                 <p className="text-[#64748B] m-2">{maindata.description}</p>
                 <div className="divider" />
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center ">
 
-                    <button className="bg-[#F1F5F9] py-1 px-3 rounded-xl ">{maindata.category}</button>
+                    <button className="bg-[#F1F5F9] py-1 px-3 rounded-xl cursor-pointer">{maindata.category}</button>
 
-                    <p className="text-[#64748B]">{maindata.difficulty}</p>
-                    <p><FaStar className="text-yellow-300" />{maindata.rating}</p>
+                    <p className="text-[#64748B] ">{maindata.difficulty}</p>
+                    <FaStar className="text-yellow-300 ml-15" /><span className="mr-4">{maindata.rating}</span>
 
 
                 </div>
 
 
-                <button className="btn btn-neutral px-30">Add to Stack</button>
+                <button
+                    className="btn btn-neutral px-30 mt-4">Add to Stack</button>
             </div>
         </div >
     );
